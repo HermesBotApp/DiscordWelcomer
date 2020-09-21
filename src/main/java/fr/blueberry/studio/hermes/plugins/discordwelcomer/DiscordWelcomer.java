52037d026f3e16aa7129f5542e5e0e2559460c32
Grimille <hello@blueberry-studio.fr>
2020-09-21 13:32:38 +0200

    Initial commit

---
+++ src/main/java/fr/blueberry/studio/hermes/plugins/discordwelcomer/DiscordWelcomer.java
@@ -0,0 +1,30 @@
+package fr.blueberry.studio.hermes.plugins.discordwelcomer;
+
+import org.simpleyaml.configuration.file.YamlFile;
+
+import fr.blueberry.studio.hermes.api.bots.IBotManager;
+import fr.blueberry.studio.hermes.api.plugins.Plugin;
+import fr.blueberry.studio.hermes.plugins.discordwelcomer.listeners.discord.GuildMemberJoinListener;
+
+public class DiscordWelcomer extends Plugin {
+
+    public static DiscordWelcomer INSTANCE;
+
+    @Override
+    public void onLoad() {
+        INSTANCE = this;
+    }
+
+    @Override
+    public void onEnable() {
+        final IBotManager botManager = getHermes().getBotManager();
+        final YamlFile config = getConfiguration();
+
+        getHermes().getBotManager().getJDAListenerManager().registerJDAListener(new GuildMemberJoinListener(botManager, config));
+    }
+
+    @Override
+    public void onDisable() {
+    }
+    
+}
---
+++ src/main/java/fr/blueberry/studio/hermes/plugins/discordwelcomer/listeners/discord/GuildMemberJoinListener.java
@@ -0,0 +1,64 @@
+package fr.blueberry.studio.hermes.plugins.discordwelcomer.listeners.discord;
+
+import org.simpleyaml.configuration.file.YamlFile;
+
+import emoji4j.EmojiUtils;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import fr.blueberry.studio.hermes.api.bots.Bot;
+import fr.blueberry.studio.hermes.api.bots.IBotManager;
+import fr.blueberry.studio.hermes.utils.MessageEmbedHelper;
+import fr.blueberry.studio.hermes.utils.RandomHelper;
+import fr.blueberry.studio.hermes.utils.ColorHelper;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class GuildMemberJoinListener extends ListenerAdapter {
+    private final IBotManager botManager;
+    private final YamlFile config;
+
+    public GuildMemberJoinListener(IBotManager botManager, YamlFile config) {
+        this.botManager = botManager;
+        this.config = config;
+    }
+
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        final ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+
+        service.schedule(() -> {
+            final Bot bot = botManager.pickRandomBot(true);
+            final TextChannel channel = event.getGuild().getTextChannelById(config.getLong("channel"));
+            final Color color = ColorHelper.toRGB(config.getString("color"));
+            final ArrayList<String> messages = (ArrayList<String>)config.getStringList("messages");
+            final int randomIndex = RandomHelper.getRandomIndex(messages);
+            final String welcome = messages.get(randomIndex).replaceAll("%mention%", event.getMember().getAsMention());
+            final MessageEmbed embed = MessageEmbedHelper.getBuilder()
+                .setDescription(welcome)
+                .setThumbnail(config.getString("image"))
+                .setTitle(config.getString("title"))
+                .setColor(color)
+                .build();
+            System.out.println("Event");
+
+            bot.getJDA().getTextChannelById(channel.getId()).sendMessage(embed).queue(message -> {
+                final String emote = config.getString("emote");
+
+                System.out.println(emote);
+                if(EmojiUtils.isEmoji(emote)) {
+                    message.addReaction(EmojiUtils.getEmoji(emote).getEmoji()).queue();
+                } else {
+                    message.addReaction(emote).queue();
+                }
+            });
+            
+        }, 1, TimeUnit.SECONDS);
+    }
+}
